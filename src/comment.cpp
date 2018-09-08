4c720125da3776e8bca332f0c7616a1ab1ce7486
Ekaterina <yukitsumetai@yandex.ru>
2018-09-08 19:06:27 +0300

    changed init_commit function

--- src/comment.cpp
+++ src/comment.cpp
@@ -27,9 +27,9 @@ commentary& the_commentary(void)
 
 // Initializes commentary data, reading it from the language.dat file
 //
-void commentary::init_commentary(string language_file)
+void commentary::init_commentary(string input_file)
 {
-    ifstream infile(language_file.c_str());
+    ifstream infile(input_file.c_str());
 
     if (!infile)
         die("Failed to open language.dat");
